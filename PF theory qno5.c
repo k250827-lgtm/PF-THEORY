b0af0eeef0781eebd37e58e163f1b9cecd8078ff
k250827-lgtm <k250827@nu.edu.pk>
2025-09-19 21:59:36 +0500

    Update PF theory qno5.c

--- PF theory qno5.c
+++ PF theory qno5.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    int count[10] = {0};  
+    int c0=0, c1=0, c2=0, c3=0, c4=0, c5=0, c6=0, c7=0, c8=0, c9=0;
     int input;
 
     printf("Enter a number between 0-9 (any other input to stop): ");
@@ -10,23 +10,38 @@ int main()
     {
         if (input < 0 || input > 9)
             break;
-        count[input]++;  
+
+        if (input == 0) c0++;
+        else if (input == 1) c1++;
+        else if (input == 2) c2++;
+        else if (input == 3) c3++;
+        else if (input == 4) c4++;
+        else if (input == 5) c5++;
+        else if (input == 6) c6++;
+        else if (input == 7) c7++;
+        else if (input == 8) c8++;
+        else if (input == 9) c9++;
+
         printf("Enter a number between 0-9 (any other input to stop): ");
     }
 
-    printf("Number\tOccurrences\n");
-    int found = 0;
-    for (int i = 0; i < 10; i++)
+    if (c0==0 && c1==0 && c2==0 && c3==0 && c4==0 && c5==0 && c6==0 && c7==0 && c8==0 && c9==0)
     {
-        if (count[i] > 0)
-        {
-            printf("%d\t%d\n", i, count[i]);
-            found = 1;
-        }
+        printf("No numbers were entered.\n");
+        return 0;
     }
 
-    if (!found)
-        printf("No numbers were entered.\n");
+    printf("Number\tOccurrences\n");
+    if (c0 > 0) printf("0\t%d\n", c0);
+    if (c1 > 0) printf("1\t%d\n", c1);
+    if (c2 > 0) printf("2\t%d\n", c2);
+    if (c3 > 0) printf("3\t%d\n", c3);
+    if (c4 > 0) printf("4\t%d\n", c4);
+    if (c5 > 0) printf("5\t%d\n", c5);
+    if (c6 > 0) printf("6\t%d\n", c6);
+    if (c7 > 0) printf("7\t%d\n", c7);
+    if (c8 > 0) printf("8\t%d\n", c8);
+    if (c9 > 0) printf("9\t%d\n", c9);
 
     return 0;
 }
